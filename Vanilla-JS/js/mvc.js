eb0d07941ecc36c1fc23a51e4b2e2a0414cd41ba
yoshuu <appcs342@gmail.com>
2022-10-31 12:04:14 +0800

    建立mvc測試用html

--- Vanilla-JS/js/mvc.js
+++ Vanilla-JS/js/mvc.js
@@ -1,9 +1,46 @@
 class Model {
-  constructor() {}
+  constructor() {
+    this.todos = [];
+  }
+
+  change(callback) {
+    this.isChange = callback;
+  }
+
+  _commit(todos) {
+    this.isChange(todos);
+  }
+
+  addTodo(todoText) {
+    const todo = {
+      id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
+      text: todoText,
+      complete: false,
+    };
+
+    this.todos.push(todo);
+    this._commit(this.todos);
+  }
 }
 
 class View {
-  constructor() {}
+  constructor() {
+    this.app = this.getElement("#root");
+    this.title = this.createElement("h1");
+    this.title.textContent = "apple";
+
+    this.app.append(this.title);
+  }
+
+  createElement(tag, className) {
+    const element = document.createElement(tag);
+    if (className) element.classList.add(className);
+    return element;
+  }
+
+  getElement(selector) {
+    return document.querySelector(selector);
+  }
 }
 
 class Controller {
